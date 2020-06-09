8d5e9c550b7778b7afa3bec556bea523f1f33045
Aditya <padalaadityavardhan@gmail.com>
2020-06-09 22:07:41 +0530

    Added debug logging

--- honggfuzz/syscall_fuzz.c
+++ honggfuzz/syscall_fuzz.c
@@ -6,6 +6,8 @@
 #include <rump/rump.h>
 #include <rump/rump_syscalls.h>
 
+#define DEBUG 1
+
 int rump_syscall(int num, void *data, size_t dlen, register_t *retval);
 
 int val,val1,Initialized=0;
@@ -29,6 +31,7 @@ LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
 	if (Size != 2 + 8 * sizeof(uint64_t))
 		  return 0;
+	
 	if(!Initialized){
 		Initialize();
 		Initialized=1;
@@ -43,6 +46,10 @@ LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 	uint64_t args[8];
 	memcpy(&args[0],Data, 8*sizeof(uint64_t));
 	
+	#ifdef DEBUG
+	printf("syscall_number: %d  args = [%ld, %ld, %ld, %ld, %ld, %ld, %ld ,%ld]", syscall_number, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+	#endif
+
 	rump_syscall(syscall_number, &args, sizeof(args), retval);
 
 	return 0;
