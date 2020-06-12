3b6cc53f65b2cb8f842032b0dafd883d6e470bbb
Aditya <padalaadityavardhan@gmail.com>
2020-06-12 08:50:12 +0530

    rumpns_ fix

--- honggfuzz/copy.c
+++ honggfuzz/copy.c
@@ -1,3 +1,8 @@
+/*
+ * hfuzz-clang -fPIC -shared copy.c -o copy.so
+ * honnggfuzz -E LD_PRELOAD=/path/to/copy.so -P -f corpus -- ./fuzzer
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,21 +11,21 @@ int raise(int num)
 {
 	exit(0);
 }
-int copyout(const void *kaddr, void *uaddr, size_t len)
+int rumpns_copyout(const void *kaddr, void *uaddr, size_t len)
 {
 	return 0;
 }
-int copyin(const void *uaddr, void *kaddr, size_t len)
-{
-	memset(kaddr, 0, len);
-	return 0;
-}
-int copyinstr(const void *uaddr, void *kaddr, size_t len, size_t *done)
+int rumpns_copyin(const void *uaddr, void *kaddr, size_t len)
 {
 	memset(kaddr, 0, len);
 	return 0;
 }
-int copyoutstr(const void *kaddr, void *uaddr, size_t len, size_t *done)
+int rumpns_copyinstr(const void *uaddr, void *kaddr, size_t len, size_t *done)
+{
+	memset(kaddr, 0, len);
+	return 0;
+}
+int rumpns_copyoutstr(const void *kaddr, void *uaddr, size_t len, size_t *done)
 {
 	return 0;
 }
